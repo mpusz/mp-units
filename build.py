810fc615b4b821f4a76e82d3668d8e12ecc883bc
Mateusz Pusz <mateusz.pusz@gmail.com>
2019-04-07 12:38:36 +0200

    Most of the environment variables moved from .travis.yml to build.py

--- build.py
+++ build.py
@@ -1,6 +1,10 @@
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(remotes="https://api.bintray.com/conan/mpusz/conan-mpusz, https://api.bintray.com/conan/martinmoene/nonstd-lite")
-    builder.add_common_builds(shared_option_name=False, pure_c=False)
+    builder = ConanMultiPackager(username="mpusz", login_username="mpusz",
+                                 channel="testing",
+                                 stable_branch_pattern="release/*",
+                                 upload="https://api.bintray.com/conan/mpusz/conan-mpusz",
+                                 remotes="https://api.bintray.com/conan/martinmoene/nonstd-lite")
+    builder.add_common_builds(pure_c=False)
     builder.run()
