234b1cf15b7cc2ae3e762b51a9510ddc2da8cef9
Mateusz Pusz <mateusz.pusz@gmail.com>
2019-04-07 09:44:56 +0200

    build.py fixed

--- build.py
+++ build.py
@@ -1,7 +1,6 @@
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name=False, pure_c=False,
-                              remotes="https://api.bintray.com/conan/mpusz/conan-mpusz, https://api.bintray.com/conan/martinmoene/nonstd-lite")
+    builder = ConanMultiPackager(remotes="https://api.bintray.com/conan/mpusz/conan-mpusz, https://api.bintray.com/conan/martinmoene/nonstd-lite")
+    builder.add_common_builds(shared_option_name=False, pure_c=False)
     builder.run()
