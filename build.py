373e70f2b169b3353cec695ea58c7b2b9cf45c2c
Mateusz Pusz <mateusz.pusz@gmail.com>
2019-04-09 13:43:53 +0200

    Conan stable_branch_pattern changed

--- build.py
+++ build.py
@@ -1,12 +1,12 @@
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="mpusz", login_username="mpusz",
-                                 channel="testing",
-                                 stable_branch_pattern="release/*",
-                                 upload="https://api.bintray.com/conan/mpusz/conan-mpusz",
-                                 remotes="https://api.bintray.com/conan/martinmoene/nonstd-lite",
-                                 build_policy="outdated",
+    builder = ConanMultiPackager(username = "mpusz", login_username = "mpusz",
+                                 channel = "testing",
+                                 stable_branch_pattern = r"v\d+\.\d+\.\d+.*",
+                                 upload = "https://api.bintray.com/conan/mpusz/conan-mpusz",
+                                 remotes = "https://api.bintray.com/conan/martinmoene/nonstd-lite",
+                                 build_policy = "outdated",
                                  upload_dependencies="all")
     builder.add_common_builds(pure_c=False)
     builder.run()
