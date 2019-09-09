be54ae510f602f8e1ad4a2a572cea465b35158e1
Mateusz Pusz <mateusz.pusz@gmail.com>
2019-09-09 23:00:14 +0200

    cmcstl2 updated to 2019.09.09

--- conanfile.py
+++ conanfile.py
@@ -45,7 +45,7 @@ class UnitsConan(ConanFile):
     exports = ["LICENSE.md"]
     settings = "os", "compiler", "build_type", "arch"
     requires = (
-        "cmcstl2/2019.08.07@mpusz/stable",
+        "cmcstl2/2019.09.09@mpusz/stable",
         "gsl-lite/0.33.0@nonstd-lite/stable"
     )
     scm = {
