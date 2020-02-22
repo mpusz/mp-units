424f0512c13132aff77ec84f24589f233c5e0ae4
Oliver Schönrock <oliver@openbrackets.net>
2020-02-22 01:44:00 +0000

    hack fix?!

--- src/include/units/bits/unit_text.h
+++ src/include/units/bits/unit_text.h
@@ -22,6 +22,9 @@
 
 #pragma once
 
+#include "units/bits/deduced_symbol_text.h"
+#include "units/bits/external/text_tools.h"
+#include "units/prefix.h"
 #include <units/derived_dimension.h>
 
 namespace units::detail {
