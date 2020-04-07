142e5d98cb8027e1a8c003f0cd49c05606fa55a9
rbrugo <brugo.riccardo@gmail.com>
2020-04-07 02:02:17 +0200

    New si.h header

---
+++ src/include/units/physical/si.h
@@ -0,0 +1,41 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2018 Mateusz Pusz
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#pragma once
+
+#include "units/physical/si/area.h"
+#include "units/physical/si/constants.h"
+#include "units/physical/si/frequency.h"
+#include "units/physical/si/momentum.h"
+#include "units/physical/si/power.h"
+#include "units/physical/si/velocity.h"
+#include "units/physical/si/volume.h"
+#include "units/physical/si/surface_tension.h"
+#include "units/physical/si/density.h"
+#include "units/physical/si/resistance.h"
+#include "units/physical/si/voltage.h"
+#include "units/physical/si/magnetic_induction.h"
+#include "units/physical/si/magnetic_flux.h"
+#include "units/physical/si/inductance.h"
+#include "units/physical/si/conductance.h"
+#include "units/physical/si/catalytic_activity.h"
+#include "units/physical/si/absorbed_dose.h"
