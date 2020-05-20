735eb3354ede86107283265a9ab82e06f4648694
Mateusz Pusz <mateusz.pusz@gmail.com>
2020-05-20 22:12:03 +0200

    experimental_angle example compilation fixed on gcc-10

--- example/experimental_angle.cpp
+++ example/experimental_angle.cpp
@@ -1,10 +1,29 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2018 Mateusz Pusz
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
-
-
+#include <units/format.h>
+#include <units/physical/si/energy.h>
 #include <units/physical/si/length.h>
 #include <units/physical/si/torque.h>
-#include <units/physical/si/energy.h>
-#include <units/format.h>
 #include <iostream>
 
 using namespace units;
@@ -12,11 +31,10 @@ using namespace units::physical::si::literals;
 
 int main()
 {
-   auto torque = 20.0q_Nm;
-   auto energy = 20.0q_J;
+  auto torque = 20.0q_Nm;
+  auto energy = 20.0q_J;
 
-   physical::Angle angle = torque/energy;
+  physical::Angle AUTO angle = torque / energy;
 
-   std::cout << angle <<'\n';
+  std::cout << angle << '\n';
 }
-
