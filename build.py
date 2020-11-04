9c6798b978b71c7f07344ed69945fc68c0c50f8a
Mateusz Pusz <mateusz.pusz@gmail.com>
2020-11-04 22:23:49 +0100

    build: Conan deployment variables are not fixed anymore

--- build.py
+++ build.py
@@ -1,9 +1,36 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2018 Mateusz Pusz
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# The following environment variables are required
+# - CONAN_USERNAME
+# - CONAN_LOGIN_USERNAME
+# - CONAN_PASSWORD
+# - CONAN_UPLOAD
+
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(
         # package id
-        username = "mpusz",
         channel = "testing",
         stable_branch_pattern = r"v\d+\.\d+\.\d+.*",
 
@@ -17,10 +44,6 @@ if __name__ == "__main__":
 
         # build configurations
         archs = ["x86_64"], #  limit to 64-bit only
-
-        # package upload (REMEMBER to set CONAN_PASSWORD environment variable in Travis CI and AppVeyor)
-        login_username = "mpusz",
-        upload = "https://api.bintray.com/conan/mpusz/conan-mpusz"
     )
     builder.add_common_builds(pure_c=False)
     for settings, options, env_vars, build_requires, reference in builder.items:
