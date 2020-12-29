dd2079055da6c1144707b2bb6b5a84099c823f40
Mateusz Pusz <mateusz.pusz@gmail.com>
2020-12-29 11:46:27 +0100

    feat: hello_units example updated with the latest changes

--- example/hello_units.cpp
+++ example/hello_units.cpp
@@ -36,17 +36,24 @@ constexpr Speed auto avg_speed(Length auto d, Time auto t)
 int main()
 {
   using namespace units::physical::si::literals;
-  Speed auto v1 = avg_speed(220_q_km, 2_q_h);
-  Speed auto v2 = avg_speed(si::length<si::international::mile>(140), si::time<si::hour>(2));
-#if UNITS_DOWNCAST_MODE == 0
-  Speed auto v3 = quantity_cast<si::dim_speed, si::metre_per_second>(v2);
-#else
-  Speed auto v3 = quantity_cast<si::metre_per_second>(v2);
-#endif
-  Speed auto v4 = quantity_cast<int>(v3);
+  using namespace units::physical::si::unit_constants;
 
-  std::cout << v1 << '\n';                             // 110 km/h
-  std::cout << fmt::format("{}", v2) << '\n';          // 70 mi/h
-  std::cout << fmt::format("{:%Q in %q}", v3) << '\n'; // 31.2928 in m/s
-  std::cout << fmt::format("{:%Q}", v4) << '\n';       // 31
+  constexpr Speed auto v1 = 110 * km / h;
+  constexpr Speed auto v2 = avg_speed(220_q_km, 2_q_h);
+  constexpr Speed auto v3 = avg_speed(si::length<si::international::mile>(140), si::time<si::hour>(2));
+#if UNITS_DOWNCAST_MODE == 0
+  constexpr Speed auto v4 = quantity_cast<si::speed<si::metre_per_second>>(v2);
+  constexpr Speed auto v5 = quantity_cast<si::dim_speed, si::metre_per_second>(v3);
+#else
+  constexpr Speed auto v4 = quantity_cast<si::speed<si::metre_per_second>>(v2);
+  constexpr Speed auto v5 = quantity_cast<si::metre_per_second>(v3);
+#endif
+  constexpr Speed auto v6 = quantity_cast<int>(v5);
+
+  std::cout << v1 << '\n';                                  // 110 km/h
+  std::cout << fmt::format("{}", v2) << '\n';               // 110 km/h
+  std::cout << fmt::format("{:*^14}", v3) << '\n';          // ***70 mi/h****
+  std::cout << fmt::format("{:%Q in %q}", v4) << '\n';      // 30.5556 in m/s
+  std::cout << fmt::format("{0:%Q} in {0:%q}", v5) << '\n'; // 31.2928 in m/s
+  std::cout << fmt::format("{:%Q}", v6) << '\n';            // 31
 }
