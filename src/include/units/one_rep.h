7be653f2a63ae62572996832e860bd5ed3b5c8a0
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2021-02-21 17:10:35 -0400

    fix: check selective QuantityValue requirements

--- src/include/units/one_rep.h
+++ src/include/units/one_rep.h
@@ -83,7 +83,9 @@ struct one_rep {
   [[nodiscard]] friend constexpr Rep operator/(one_rep, const Rep&) = delete;
 
   template<typename Rep>
-    requires requires { quantity_values<Rep>::one(); }
+    requires requires { quantity_values<Rep>::one(); requires !Quantity<Rep> && // TODO: Replace with `QuantityValue`
+                                                              !QuantityLike<Rep> && // when Clang catches up.
+                                                              !wrapped_quantity_<Rep>; }
   [[nodiscard]] constexpr operator Rep() const noexcept
   {
     return quantity_values<Rep>::one();
