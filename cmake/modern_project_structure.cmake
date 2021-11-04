54457b2457ea76fd5263b498e4f4a342885adfef
Mateusz Pusz <mateusz.pusz@gmail.com>
2021-11-04 12:24:34 +0100

    style: typo fixed

--- cmake/modern_project_structure.cmake
+++ cmake/modern_project_structure.cmake
@@ -26,6 +26,6 @@ function(ensure_entry_point)
   if(NOT CMAKE_CURRENT_SOURCE_DIR STREQUAL CMAKE_SOURCE_DIR)
       message(FATAL_ERROR "'${CMAKE_CURRENT_SOURCE_DIR}/CMakeLists.txt' is meant to be used only "
               "as a CMake entry point and should not be included from other CMake files. "
-              "Include '${CMAKE_CURRENT_SOURCE_DIR}/src/CMaskeLists.txt' directly instead.")
+              "Include '${CMAKE_CURRENT_SOURCE_DIR}/src/CMakeLists.txt' directly instead.")
   endif()
 endfunction()
