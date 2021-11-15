7be9f310f17e00d0daa180800aa89a89fe8c65ad
Markus Hofbauer <markus.hofbauer@tum.de>
2021-11-15 11:44:55 +0100

    be specific for msvc

--- test/unit_test/runtime/math_test.cpp
+++ test/unit_test/runtime/math_test.cpp
@@ -149,7 +149,8 @@ TEST_CASE("floor functions", "[floor]")
     REQUIRE(floor<si::second>(-999._q_ms) == -1_q_s);
   }
   SECTION ("floor 1 second with target quantity with unit type second should be 1 second") {
-    REQUIRE(floor<si::time<si::second, long int>>(1_q_s) == 1_q_s);
+    using showtime = si::time<si::second, int>;
+    REQUIRE(floor<showtime>(showtime::one()) == showtime::one());
   }
 }
 
@@ -193,7 +194,8 @@ TEST_CASE("ceil functions", "[ceil]")
     REQUIRE(ceil<si::second>(-999._q_ms) == 0_q_s);
   }
   SECTION ("ceil 1 second with target quantity with unit type second should be 1 second") {
-    REQUIRE(ceil<si::time<si::second, long int>>(1_q_s) == 1_q_s);
+    using showtime = si::time<si::second, int>;
+    REQUIRE(ceil<showtime>(showtime::one()) == showtime::one());
   }
 }
 
