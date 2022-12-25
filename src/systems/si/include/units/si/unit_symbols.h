9e5a95edefb59c09e1b83d0d74c442f6e2dd9079
Mateusz Pusz <mateusz.pusz@gmail.com>
2022-12-25 17:33:47 +0100

    feat: `deg` unit symbol added to SI

--- src/systems/si/include/units/si/unit_symbols.h
+++ src/systems/si/include/units/si/unit_symbols.h
@@ -624,6 +624,7 @@ inline constexpr auto Ykat = yotta<katal>;
 
 // TODO Should the following non-SI units have prefixes symbols predefiend as well?
 inline constexpr auto au = astronomical_unit;
+inline constexpr auto deg = degree;
 inline constexpr auto a = are;
 inline constexpr auto ha = hectare;
 inline constexpr auto l = litre;
