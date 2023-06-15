7a9800a8fa45539c21bcac141723e08a28477555
Mateusz Pusz <mateusz.pusz@gmail.com>
2023-06-15 11:26:37 +0300

    build: `fmt` updated to `10.0.0`

--- conanfile.py
+++ conanfile.py
@@ -109,7 +109,7 @@ class MPUnitsConan(ConanFile):
     def requirements(self):
         self.requires("gsl-lite/0.40.0")
         if self._use_libfmt:
-            self.requires("fmt/8.1.1")
+            self.requires("fmt/10.0.0")
         if self._use_range_v3:
             self.requires("range-v3/0.11.0")
 
