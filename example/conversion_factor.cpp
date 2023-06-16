5ca185640836caa1caa5f900f53f854385f7dc0b
Mateusz Pusz <mateusz.pusz@gmail.com>
2023-06-16 13:54:45 +0300

    fix(example): `value_cast` prefixed with a namespace in `conversion_factor` example

--- example/conversion_factor.cpp
+++ example/conversion_factor.cpp
@@ -33,7 +33,7 @@ template<mp_units::Quantity Target, mp_units::Quantity Source>
   requires std::constructible_from<Target, Source>
 inline constexpr double conversion_factor(Target, Source)
 {
-  return value_cast<Target::unit>(1. * Source::reference).number();
+  return mp_units::value_cast<Target::unit>(1. * Source::reference).number();
 }
 
 }  // namespace
