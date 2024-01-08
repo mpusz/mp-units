ffc8b6bd0d09d412aa25ae8b88209e4b5e3cdbf2
Mateusz Pusz <mateusz.pusz@gmail.com>
2024-01-08 13:33:00 +0100

    test: system header files simplified in test_package

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -22,8 +22,8 @@
 
 #include <mp-units/format.h>
 #include <mp-units/ostream.h>
-#include <mp-units/systems/isq/space_and_time.h>
-#include <mp-units/systems/si/unit_symbols.h>
+#include <mp-units/systems/isq/isq.h>
+#include <mp-units/systems/si/si.h>
 #include <iostream>
 
 using namespace mp_units;
