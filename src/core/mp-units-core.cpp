fbe3cec982bddd673deaefa21a149f4fe0c576a2
Mateusz Pusz <mateusz.pusz@gmail.com>
2024-04-25 16:09:00 +0200

    feat: :boom: `core.h` removed

--- src/core/mp-units-core.cpp
+++ src/core/mp-units-core.cpp
@@ -6,4 +6,10 @@ export module mp_units.core;
 
 #define MP_UNITS_IN_MODULE_INTERFACE
 
-#include <mp-units/core.h>
+#include <mp-units/compat_macros.h>
+#include <mp-units/concepts.h>
+#include <mp-units/format.h>
+#include <mp-units/framework.h>
+#include <mp-units/math.h>
+#include <mp-units/ostream.h>
+#include <mp-units/random.h>
