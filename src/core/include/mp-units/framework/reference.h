e08a0ace248b0e6b33d77aa542dda0240f159a2c
Mateusz Pusz <mateusz.pusz@gmail.com>
2024-06-20 10:19:22 +0200

    fix: missing header file include added

--- src/core/include/mp-units/framework/reference.h
+++ src/core/include/mp-units/framework/reference.h
@@ -26,6 +26,7 @@
 #include <mp-units/bits/get_associated_quantity.h>
 #include <mp-units/bits/module_macros.h>
 #include <mp-units/framework/quantity_concepts.h>
+#include <mp-units/framework/quantity_point_concepts.h>
 #include <mp-units/framework/reference_concepts.h>
 #include <mp-units/framework/representation_concepts.h>
 
