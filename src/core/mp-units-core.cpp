8f062bfa87562056f4d43bcf14ddf14a5400f729
Mateusz Pusz <mateusz.pusz@gmail.com>
2024-11-07 10:24:28 +0100

    refactor: mp_units.core defined in terms of `core.h`

--- src/core/mp-units-core.cpp
+++ src/core/mp-units-core.cpp
@@ -32,13 +32,4 @@ import std;
 
 #define MP_UNITS_IN_MODULE_INTERFACE
 
-#include <mp-units/compat_macros.h>
-#include <mp-units/concepts.h>
-#include <mp-units/framework.h>
-
-#if MP_UNITS_HOSTED
-#include <mp-units/format.h>
-#include <mp-units/math.h>
-#include <mp-units/ostream.h>
-#include <mp-units/random.h>
-#endif
+#include <mp-units/core.h>
