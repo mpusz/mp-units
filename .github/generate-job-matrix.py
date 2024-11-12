e464677200cf8df116d05335370096696c5f6c28
Yves Delley <delleyves@gmx.ch>
2024-11-12 18:58:06 +0100

    don't test Clang 19 just yet; fix cancel-in-progres

--- .github/generate-job-matrix.py
+++ .github/generate-job-matrix.py
@@ -89,7 +89,7 @@ configs = {
     for c in [make_gcc_config(ver) for ver in [12, 13, 14]]
     + [
         make_clang_config(ver, platform)
-        for ver in [16, 17, 18, 19]
+        for ver in [16, 17, 18]
         for platform in ["x86-64", "arm64"]
     ]
     + [make_apple_clang_config(ver) for ver in [15]]
