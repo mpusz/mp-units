2a2a53d5e8e4e754959f2f7cb2cf41c71bf54efa
Mateusz Pusz <mateusz.pusz@gmail.com>
2025-11-02 15:08:20 -0800

    build: projectPrefix CMake variable removed

--- src/cmake/CheckCacheVarValues.cmake
+++ src/cmake/CheckCacheVarValues.cmake
@@ -23,10 +23,10 @@
 cmake_minimum_required(VERSION 3.25)
 
 function(check_cache_var_values name)
-    set_property(CACHE ${projectPrefix}${name} PROPERTY STRINGS ${ARGN})
-    if(NOT ${projectPrefix}${name} IN_LIST ARGN)
+    set_property(CACHE ${name} PROPERTY STRINGS ${ARGN})
+    if(NOT ${name} IN_LIST ARGN)
         message(FATAL_ERROR
-                    "Invalid value '${${projectPrefix}${name}}' provided for a cache variable ${projectPrefix}${name} (${ARGN} allowed)"
+                    "Invalid value '${${name}}' provided for a cache variable ${name} (${ARGN} allowed)"
         )
     endif()
 endfunction()
