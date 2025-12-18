93f192816682ff9d82621dca83fcb384f4bdc2af
Mateusz Pusz <mateusz.pusz@gmail.com>
2025-12-18 19:07:33 +0100

    build: fmt bumped to 12.1.0

--- conanfile.py
+++ conanfile.py
@@ -211,7 +211,7 @@ class MPUnitsConan(ConanFile):
             elif self.options.contracts == "ms-gsl":
                 self.requires("ms-gsl/4.2.0", transitive_headers=True)
             if not self.options.std_format:
-                self.requires("fmt/12.0.0", transitive_headers=True)
+                self.requires("fmt/12.1.0", transitive_headers=True)
 
     def build_requirements(self):
         self.tool_requires("cmake/[>=4.2.1 <5]")
